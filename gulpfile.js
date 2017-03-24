7336c8ae0ec7dc57e1294787bd93d120a4b2dde3
riodel27 <riopamaong@gmail.com>
2017-03-24 21:26:11 +0800

    build gulp, gulp-sass,browser-sync task

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('server', function(){
+	browserSync.init({
+		server: './'
+	});
+});
+
+gulp.task('sass', function(){
+	return gulp.src('app/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['server'], function(){
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('*.html').on('change', reload);
+});
